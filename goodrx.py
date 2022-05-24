33521e79431c39a81c551500b0153d2114a68642
jleon4 <aleon1996@gmail.com>
2022-05-23 22:27:04 -0500

    initial attempt at scraper

--- goodrx.py
+++ goodrx.py
@@ -2,16 +2,35 @@
 import os
 import time
 import requests
+import csv
 from bs4 import BeautifulSoup
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.keys import Keys
 
 #global variables
-driver = webdriver.Chrome()
-email = ''
-firstName = ''
-lastName = ''
+site = "https://www.goodrx.com"
+driver = webdriver.Chrome(ChromeDriverManager().install())
 
-print("Hello")
+def get_data(query):
+    search_bar = driver.find_element_by_xpath('/html/body/div[2]/div[1]/div[1]/div[2]/div/form/div/div/input')
+    search_bar.send_keys('Test Input')
+    search_bar.send_keys(Keys.ENTER)
 
-driver.get("https://www.python.org")
+def get_query():
+    my_file = open("src/drug_query.txt", "r")
+    drug_query = my_file.readlines()
+    drug_query = list(map(lambda x:x.strip(),drug_query))
+    print(drug_query)
+    return drug_query
+
+def main():
+    print("Scraping GoodRx Site for the following: ")
+    query = get_query()
+    print("Navigating to Site")
+    driver.get(site)
+    get_data(query)
+
+if __name__ == "__main__":
+    main()
